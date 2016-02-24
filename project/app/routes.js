0feab2a6f90d21d81191523955af2eed0a38b473
Benno Meysmans <benno_njord@hotmail.com>
2016-02-24 12:12:58 +0100

    Added missing dependancy in package.json and some more scaffolding.

--- project/app/routes.js
+++ project/app/routes.js
@@ -8,4 +8,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
       templateUrl: "app/pages/home.html",
       controller: "homeController as home"
     })
+  	.state('test', {
+      url: "/test",
+      templateUrl: "app/pages/test.html",
+      controller: "testController as test"
+    })
 });
