0795b145fca029a532efb058d60e1ab69f1c3d96
Benno Meysmans <benno_njord@hotmail.com>
2016-02-24 12:15:47 +0100

    Forgot testing the service

--- project/app/controllers/homeController.js
+++ project/app/controllers/homeController.js
@@ -1,10 +1,12 @@
-app.controller("homeController", function() {
+app.controller("homeController", function(testService) {
 
 	var vm = this;
+	var tstSvc = testService;
 
 	function _init() {
 
 		vm.test = "This value has been initiated through _init() and bound to the scope!";
+		vm.svcTest = tstSvc.testValue;
 	}
 
 	_init();
